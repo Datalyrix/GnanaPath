71b60ffdc6f94a46746fa69cbceab65d065c78d1
rvchamarti <ravi.chamarti@gmail.com>
2021-10-09 10:48:43 +0000

    Modify config changes

--- gngraph/config/gngraph_config.py
+++ gngraph/config/gngraph_config.py
@@ -8,6 +8,7 @@ class GNGraphConfig:
         self.gndata_graph_config_folder = configpath
         self.gnnode_id_fpath = configpath+"/gnnode_id.pkl"
         self.gnedge_id_fpath = configpath+"/gnedge_id.pkl"
+        self.gnbizrule_id_fpath = configpath+"/gnbizrule_id.pkl"
         
 
     def save_nodeid_max(self, node_id_val):
@@ -32,4 +33,17 @@ class GNGraphConfig:
         if not os.path.exists(self.gnedge_id_fpath):
             return 500000
         with open(self.gnedge_id_fpath, 'rb') as fs:
+            return pickle.load(fs)
+
+        
+    def save_bizr_relid_max(self, bizrid_val):
+        ##data_file_name="gnedgeid.pkl"
+        with open(self.gnbizrule_id_fpath, 'wb') as fs:
+           pickle.dump(bizrid_val, fs)
+
+    def get_bizr_relid_max(self):
+        ###data_file_name="gnedgeid.pkl"
+        if not os.path.exists(self.gnbizrule_id_fpath):
+            return 900000
+        with open(self.gnbizrule_id_fpath, 'rb') as fs:
             return pickle.load(fs)
