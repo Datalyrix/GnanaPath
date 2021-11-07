02540a8dc6aec3e848d5ce30eced64ce775b57b0
rvchamarti <ravi.chamarti@gmail.com>
2021-11-07 07:04:41 -0500

    Update startup script to start container

--- gn_docker_create_run.sh
+++ gn_docker_create_run.sh
@@ -2,5 +2,5 @@
 
 docker build -t gnp .
 
-#docker run -it --name gnp_test -p 5050:5050 gnp
+docker run -d --name gnp_test -p 5050:5050 gnp
 #docker run -it --name gnp_test -v ./:/opt/GNPathDev -p 5050:5050 gnp
