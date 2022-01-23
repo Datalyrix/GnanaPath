e6f3ba9303c34993b1f8ec00969231eb546f7a25
rvchamarti <ravi.chamarti@gmail.com>
2022-01-23 13:22:02 -0500

    Update startgnpath shell

--- startgnpath.sh
+++ startgnpath.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+echo "GnPath  Debug Mode:$GN_DEBUG_MODE"
+
+if [ ! -z "$GN_DEBUG_MODE" ]
+then
+    echo " Running in command debug mode "
+    /bin/bash
+fi
+
+echo "Starting GnPath Application Server"
 cd /opt/GnanaPath/gnappsrv
 nohup python gnp_appsrv_main.py
-/bin/bash
-echo "GnanaPath has started " >/var/log/all
